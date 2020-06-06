a7c8cadcd42df19b6e3aff950ecfd3a2f2c274f8
Gürol Sağlam <gurol.saglam@ozu.edu.tr>
2020-06-06 04:51:24 +0300

    Input format changed
    Input format changed into a file with node to node connection information and the car routes. Object oriented stuff added for initializing Graph.

--- project.py
+++ project.py
@@ -2,8 +2,9 @@
 # You will need dwave_qbsolv and osmnx libraries to be able to use this program.
 # Please use these lines below to install these packages:
 # pip install dwave_qbsolv
-# pip install osmnx
 
+# NOT REQUIRED
+# pip install osmnx
 # in order for osmnx to be downloaded, you may have to install rtree from:
 # https://www.lfd.uci.edu/~gohlke/pythonlibs/#rtree
 # you may also have to install GDAL-API from:
@@ -11,14 +12,141 @@
 # this link is how to do it on windows: https://sandbox.idre.ucla.edu/sandbox/tutorials/installing-gdal-for-windows
 # if even though GDAL is installed and Fiona still does not install, get the whl for Fiona from here:
 # https://www.lfd.uci.edu/~gohlke/pythonlibs/#fiona
+#
+# HOW TO USE
+#
+# On command window:
+#    project.py example.txt
 
 
+import sys
 from dwave_qbsolv import QBSolv
+
+class Node:
+    
+    def __init__(self, nodeID):
+        self.__nodeID = nodeID
+        self.__edges = []
+    
+    def getID(self):
+        return self.__nodeID
+    
+    def getEdges(self):
+        return self.__edges
+    
+    def hasEdge(self, edge):
+        for i in range(0, len(self.__edges)):
+            if (self.__edges[i].getID() == edge.getID()):
+                return True
+        return False
+    
+    def addEdge(self, edge):
+        if (self.hasEdge(edge)):
+            return False
+        self.__edges.append(edge)
+        edge.addNode(self)
+        return True
+    
+    def equals(self, node):
+        if (node.getID() is self.getID()):
+            return True
+        return False
+    
+    def getEdgeWithNode(self, node):
+        for i in range(0, len(self.__edges)):
+            if (self.__edges[i].getOtherNode(self).equals(node)):
+                return self.__edges[i]
+        return None
+
+
+class Edge:
+    
+    def __init__(self, edgeID, nodeA = None, nodeB = None):
+        self.__edgeID = edgeID
+        self.__nodeA = nodeA
+        self.__nodeB = nodeB
+        if not (nodeA is None):
+            nodeA.addEdge(self)
+        if not (nodeB is None):
+            nodeB.addEdge(self)
+    
+    def getOtherNode(self, node):
+        if (self.__nodeA is node):
+            return self.__nodeB
+        elif (self.__nodeB is node):
+            return self.__nodeA
+        return None
+    
+    def getID(self):
+        return self.__edgeID
+    
+    def getNodes(self):
+        return (self.__nodeA, self.__nodeB)
+    
+    def addNode(self, node):
+        if (self.__nodeA is None):
+            self.__nodeA = node
+            node.addEdge(self)
+            return True
+        elif (self.__nodeB is None and not self.__nodeA.getID() == node.getID()):
+            self.__nodeB = node
+            node.addEdge(self)
+            return True
+        return False
+    
+    def equals(self, edge):
+        if (edge.getID() is self.getID()):
+            return True
+        return False
+
+class Graph:
+    
+    def __init__(self):
+        self.__nodes = []
+        self.__edges = []
+    
+    def addNode(self, node):
+        self.__nodes.append(node)
+    
+    def addNodes(self, nodes):
+        self.__nodes.extend(nodes)
+    
+    def addEdge(self, edge):
+        self.__edges.append(edge)
+    
+    def addEdges(self, edges):
+        self.__edges.extend(edges)
+    
+    def getNodes(self):
+        return self.__nodes
+    
+    def getEdges(self):
+        return self.__edges
+    
+    def getNodeWithID(self, nodeID):
+        for i in range(0, len(self.__nodes)):
+            if (self.__nodes[i].getID() is nodeID):
+                return self.__nodes[i]
+        return None
+    
+    def getEdgeWithID(self, edgeID):
+        for i in range(0, len(self.__edges)):
+            if (self.__edges[i].getID() is edgeID):
+                return self.__edges[i]
+        return None
+    
+    
+
+def isRoadInRoute(road, route2):
+    for i in range(0, len(route2)):
+        if (road.equals(route2[i])):
+            return True
+    return False
 
 def getIntersections(route1, route2):
     intersections = []
     for i in range(0, len(route1)):
-        if (route1[i] in route2):
+        if (route1[i] in route2): #isRoadInRoute(route1[i], route2)):
             intersections.append(route1[i])
     return intersections
 
@@ -29,7 +157,7 @@ def getUniqueArr(arr):
             ansArr.append(arr[i])
     return ansArr
 
-def getQuboMatrix(carRoutes):
+def getQuboMatrix(carRoutes, numOfAlternateRoutes):
     #getting the Q (qubo matrix) from carRoutes array (list)
     Q = {}
     for i in range(0, len(carRoutes)*numOfAlternateRoutes): # number of cars * number of alternate routes
@@ -65,36 +193,128 @@ def getQuboMatrix(carRoutes):
     return Q
 
 
+# AN OLDER EXAMPLE
 #                                                 original routes                     2 alternate routes
 #  _ _ _ _    (road segment IDs) _ 0 1 2 3        1st car = [0, 5, 14, 19, 20, 21]    [0, 1, 2, 3, 8, 17], [4, 9, 10, 11, 16, 21]
 # |_|_|_|_|   |4 5 6 7 8, _ 9 10 11 12            2nd car = [13, 18, 19]              [9, 10, 15], [9, 14, 19]
 # |_|_|_|_|   |13 14 15 16 17, _ 18 19 20 21      3rd car = [6, 15, 20, 21]           [2, 3, 8, 17], [2, 7, 16, 21]
+#
+# largestSegmentID = 21
+# numOfAlternateRoutes = 3
 
-largestSegmentID = 21
-numOfAlternateRoutes = 3
+# firstCarRoutes = [[0, 5, 14, 19, 20, 21], [0, 1, 2, 3, 8, 17], [4, 9, 10, 11, 16, 21]]
+# secondCarRoutes = [[13, 18, 19], [9, 10, 15], [9, 14, 19]]
+# thirdCarRoutes = [[6, 15, 20, 21], [2, 3, 8, 17], [2, 7, 16, 21]]
 
-firstCarRoutes = [[0, 5, 14, 19, 20, 21], [0, 1, 2, 3, 8, 17], [4, 9, 10, 11, 16, 21]]
-secondCarRoutes = [[13, 18, 19], [9, 10, 15], [9, 14, 19]]
-thirdCarRoutes = [[6, 15, 20, 21], [2, 3, 8, 17], [2, 7, 16, 21]]
+# carRoutes = [firstCarRoutes, secondCarRoutes, thirdCarRoutes]
 
-carRoutes = [firstCarRoutes, secondCarRoutes, thirdCarRoutes]
 
-Q = getQuboMatrix(carRoutes)
-# don't use the one below, there are small mistakes in the biases
-# Q = {(0, 0): -6, (1, 1): -5, (2, 2): -6, (3, 3): -11, (4, 4): -9, (5, 5): -9, (6, 6): -8, (7, 7): -8, (8, 8): -8, (0, 1): 24, (0, 2): 24, (1, 2): 24, (3, 4): 24, (3, 5): 24, (4, 5): 24, (6, 7): 24, (6, 8): 24, (7, 8): 24, (0, 3): 2, (0, 5): 4, (0, 6): 4, (0, 8): 2, (1, 7): 8, (1, 8): 2, (2, 4): 4, (2, 5): 2, (2, 6): 2, (2, 8): 4, (4, 6): 2}
 
-response = QBSolv().sample_qubo(Q)
-result = list(response.samples())
-result = result[0]
-resultingEnergy = list(response.data_vectors['energy'])
-resultingEnergy = resultingEnergy[0]
+def main(argv):
+    
+    file = open(argv[1])
+    lines = file.readlines()
+    file.close()
+    
+    #delete any comments and empty lines
+    for i in range(len(lines)-1, -1, -1):
+        #delete the newline character at the end
+        temp = lines[i].split("\n")
+        lines[i] = temp[0]
+        
+        temp = lines[i].split(" #")
+        lines[i] = temp[0]
+        temp = lines[i].split("#")
+        lines[i] = temp[0]
+        
+        if (lines[i] is ""):
+            tmp = lines.pop(i)
+    
+    numOfNodes = int(lines[0])
+    
+    #initialize graph and nodes
+    graph = Graph()
+    for i in range(0, numOfNodes):
+        node = Node(i)
+        graph.addNode(node)
+    
+    #initialize edges
+    nextLine = 1
+    numOfEdges = 0
+    for i in range(0, numOfNodes):
+        nodeIDs = lines[nextLine+i].split(" ")
+        for j in range(0, len(nodeIDs)):
+            nodeA = graph.getNodeWithID(i)
+            nodeB = graph.getNodeWithID(int(nodeIDs[j]))
+            
+            edge_existing = nodeA.getEdgeWithNode(nodeB)
+            if not (edge_existing is None):
+                continue
+            edge_existing = nodeB.getEdgeWithNode(nodeA)
+            if not (edge_existing is None):
+                continue
+            
+            edge = Edge(numOfEdges, nodeA, nodeB)
+            graph.addEdge(edge)
+            numOfEdges = numOfEdges + 1
+    
+    #print the edges
+    # edges = graph.getEdges()
+    # for i in range(0, len(edges)):
+        # print(str(edges[i].getID()) + "  A: " + str(edges[i].getNodes()[0].getID()) + "  B: " + str(edges[i].getNodes()[1].getID()))
+    
+    nextLine = nextLine + numOfNodes
+    numOfCars = int(lines[nextLine])
+    nextLine = nextLine + 1
+    
+    #get original car routes for each car
+    carRoutes = []
+    for i in range(0, numOfCars):
+        carRoutes.append([])
+    
+    for i in range(0, numOfCars):
+        nodeIDs = lines[nextLine+i].split("-")
+        route = []
+        for j in range(0, len(nodeIDs)-1):
+            nodeA = graph.getNodeWithID(int(nodeIDs[j]))
+            nodeB = graph.getNodeWithID(int(nodeIDs[j+1]))
+            edge = nodeA.getEdgeWithNode(nodeB)
+            route.append(edge)
+        carRoutes[i].append(route)
+    nextLine = nextLine + numOfCars
+    
+    #get the alternate routes
+    numOfAlternateRoutes = 3
+    for i in range(0, numOfCars*(numOfAlternateRoutes-1)):
+        carNum = int(lines[nextLine+i].split(" ")[0])
+        nodeIDs = (lines[nextLine+i].split(" ")[1]).split("-")
+        route = []
+        for j in range(0, len(nodeIDs)-1):
+            nodeA = graph.getNodeWithID(int(nodeIDs[j]))
+            nodeB = graph.getNodeWithID(int(nodeIDs[j+1]))
+            edge = nodeA.getEdgeWithNode(nodeB)
+            route.append(edge)
+        carRoutes[carNum].append(route)
+    
 
-# print("samples=" + str(result))
-# print("energies=" + str(list(response.data_vectors['energy'])))
+    Q = getQuboMatrix(carRoutes, numOfAlternateRoutes)
+    
+    response = QBSolv().sample_qubo(Q)
+    result = list(response.samples())
+    result = result[0]
+    resultingEnergy = list(response.data_vectors['energy'])
+    resultingEnergy = resultingEnergy[0]
 
-print("roads taken:")
-for i in range(0, len(result)):
-    if (result[i] == 1):
-        print("car " + str((i//numOfAlternateRoutes)+1) + ": route " + str((i%numOfAlternateRoutes)+1))
+    # print("samples=" + str(result))
+    # print("energies=" + str(list(response.data_vectors['energy'])))
 
-print("Resulting energy: " + str(resultingEnergy))
+    print("roads taken:")
+    for i in range(0, len(result)):
+        if (result[i] == 1):
+            print("car " + str((i//numOfAlternateRoutes)+1) + ": route " + str((i%numOfAlternateRoutes)+1))
+
+    print("Resulting energy: " + str(resultingEnergy))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
